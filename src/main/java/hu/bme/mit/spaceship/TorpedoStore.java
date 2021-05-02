842413ece639d8bb1ab7b5ad3b1ae07614fedb13
simond99 <simond@edu.bme.hu>
2021-05-02 16:32:56 +0200

    Another edit in solutions

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,7 +11,6 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-
   private int torpedoCount = 0;
 
   public TorpedoStore(int numberOfTorpedos){
@@ -34,9 +33,7 @@ public class TorpedoStore {
       throw new IllegalArgumentException("numberOfTorpedos");
 
     }
-
     boolean success = false;
-
     // simulate random overheating of the launcher bay which prevents firing
     //fix random
     double r = new Random().nextDouble();
@@ -50,7 +47,6 @@ public class TorpedoStore {
       // simulated failure
       success = false;
     }
-
     return success;
   }
 
